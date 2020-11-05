1f012e730187763c8a2ac4508252d148d75d5788
jaandreu <jaandreu@gtt.es>
2020-11-05 16:30:42 +0100

    Añadido francia

--- constants.js
+++ constants.js
@@ -25,13 +25,13 @@ const urlsDodax = [
   //   electronica: "/de-de/elektronik-cPPEA17VBM7",
   //   all: "/de-de/search/"
   //   },
-  // {
-  //   text: "FR",
-  //   url: "https://www.dodax.fr",
-  //   musica: "/fr-fr/musique-cd-dvd-vinyles-cLOFV6FFU7H",
-  //   electronica: "/fr-fr/elektronik-cPPEA17VBM7",
-  //   all: "/fr-fr/search/"
-  // },
+   {
+     text: "FR",
+     url: "https://www.dodax.fr",
+     musica: "/fr-fr/musique-cd-dvd-vinyles-cLOFV6FFU7H",
+     electronica: "/fr-fr/elektronik-cPPEA17VBM7",
+     all: "/fr-fr/search/"
+   },
    {
      text: "UK",
      url: "https://www.dodax.co.uk",
